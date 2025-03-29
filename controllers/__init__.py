e28c91045de4ad2171821a0640e0af9a4b8e4dd1
25dp1000026 <25dp1000026@ds.study.iitm.ac.in>
2025-03-29 14:11:48 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ controllers/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__, template_folder=os.path.abspath('./templates'),  static_folder=os.path.abspath('./static'))
+    app.config['SECRET_KEY'] = 'supersecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///QuizDatabase.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from models.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    with app.app_context():
+        db.create_all()
+        if not User.query.filter_by(email="Admin123@gmail.com").first():
+            admin = User(email="Admin123@gmail.com", password="admin123", is_admin=True)
+            db.session.add(admin)
+            db.session.commit()
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
---
+++ models/models.py
@@ -0,0 +1,50 @@
+from controllers import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100))
+    qualification = db.Column(db.String(100))
+    dob = db.Column(db.Date)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    chapters = db.relationship('Chapter', backref='subject', lazy=True)
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'))
+    quizzes = db.relationship('Quiz', backref='chapter', lazy=True)
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'))
+    date_of_quiz = db.Column(db.Date)
+    duration = db.Column(db.String(10))
+    questions = db.relationship('Question', backref='quiz', lazy=True)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    question_text = db.Column(db.Text, nullable=False)
+    option1 = db.Column(db.String(200))
+    option2 = db.Column(db.String(200))
+    option3 = db.Column(db.String(200))
+    option4 = db.Column(db.String(200))
+    correct_option = db.Column(db.Integer, nullable=False)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    score = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+    user = db.relationship('User', backref='scores')
+    quiz = db.relationship('Quiz', backref='scores')
