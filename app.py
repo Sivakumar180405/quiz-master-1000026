1d8df111b3346eac9aba0a1441e0beded3b51977
Sivakumar <157525476+Sivakumar180405@users.noreply.github.com>
2025-09-16 13:43:03 +0530

    For render

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from controllers import create_app, db
+import os
 
 app = create_app()
 
@@ -6,4 +7,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
